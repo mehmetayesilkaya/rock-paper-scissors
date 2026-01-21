9ceccd6d5420e930b403d6b723f7f035b0c12ad8
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-01-21 18:13:11 +0300

    feat: add game logic to compare human and computer choices

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const value = Math.floor(Math.random() * 3);
   if (value === 0) {
@@ -16,5 +19,26 @@ function getHumanChoice() {
   return choice;
 }
 
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! Both chose ${humanChoice}`);
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    ++humanScore;
+    console.log(`You win! ${humanChoice} beats ${computerChoice} `);
+  } else {
+    ++computerScore;
+    console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
 //console.log(getComputerChoice());
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
